414e744ec526a01252203107d8670f4d8bc99679
Igor Morozov <morozovigorq@gmail.com>
2025-04-12 21:20:47 +0300

    fix: try upload image

--- src/core/minio/minio.service.ts
+++ src/core/minio/minio.service.ts
@@ -1,7 +1,14 @@
 import { Client } from 'minio'
-import { Inject, Injectable } from '@nestjs/common'
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common'
 
 @Injectable()
-export class MinioService {
+export class MinioService implements OnModuleInit {
   constructor(@Inject('MINIO') readonly minio: Client) {}
+
+  async onModuleInit() {
+    const exists = await this.minio.bucketExists('projects')
+    if (!exists) {
+      await this.minio.makeBucket('projects')
+    }
+  }
 }
