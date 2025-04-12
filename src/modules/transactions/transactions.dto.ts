17dcb54ecd53304cdbdb3342c9b57c3b947dfdd7
Igor Morozov <morozovigorq@gmail.com>
2025-04-12 16:35:05 +0300

    feat: add user balance

--- src/modules/transactions/transactions.dto.ts
+++ src/modules/transactions/transactions.dto.ts
@@ -1,10 +1,11 @@
+import { TransactionType } from '@prisma/client'
 import { createZodDto } from 'nestjs-zod'
 import { z } from 'zod'
 
 const CreateTransactionSchema = z.object({
   amount: z.number().int().min(100),
-  projectId: z.string().uuid(),
-  // type
+  projectId: z.string().uuid().optional(),
+  type: z.nativeEnum(TransactionType),
 })
 
-export class CreateTransactionDto extends createZodDto(CreateTransactionSchema) {}
+export class CreateTransactionDto extends createZodDto(CreateTransactionSchema) { }
--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -8,13 +8,14 @@ import {
 import { PrismaService } from 'src/core/prisma/prisma.service'
 import { IdDto, ListDto } from 'src/shared/dtos'
 import { CreateTransactionDto } from './transactions.dto'
+import { TransactionType } from '@prisma/client'
 
 @Injectable()
 export class TransactionsService {
   private readonly logger = new Logger(TransactionsService.name)
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
-  async list(userId: string, { limit: take, offset: skip }: ListDto) {
+  async list (userId: string, { limit: take, offset: skip }: ListDto) {
     const transactions = await this.prisma.transaction.findMany({
       take,
       skip,
@@ -37,7 +38,7 @@ export class TransactionsService {
     const transactionsResponse = transactions.map((transaction) => {
       return {
         id: transaction.id,
-        project: transaction.project.title,
+        project: transaction.project?.title,
         date: transaction.updatedAt,
         // type
         // status
@@ -46,7 +47,7 @@ export class TransactionsService {
     return { transactions: transactionsResponse }
   }
 
-  async get(userId: string, { id }: IdDto) {
+  async get (userId: string, { id }: IdDto) {
     const transaction = await this.prisma.transaction.findUnique({
       where: {
         id,
@@ -56,25 +57,72 @@ export class TransactionsService {
     return { transaction }
   }
 
-  async createTransaction(userId: string, { amount, projectId }: CreateTransactionDto) {
+  async createTransaction (userId: string, { amount, projectId, type }: CreateTransactionDto) {
     try {
       const transaction = await this.prisma.$transaction(async (prisma) => {
-        const project = await prisma.project.findUnique({
-          where: {
-            id: projectId,
-          },
-        })
-        if (!project) throw new BadRequestException('Project does not exists')
-        const transaction = await this.prisma.transaction.create({
-          data: {
-            amount,
-            projectId,
-            userId,
-            // обновление суммы в project
-            // type
-            // status: 'completed'
-          },
-        })
+        let transaction
+        if (type === TransactionType.donation) {
+          const project = await prisma.project.findUnique({
+            where: {
+              id: projectId ?? '-1',
+              endDate: {
+                gte: new Date(),
+              },
+            },
+          })
+          if (!project) throw new BadRequestException('Project does not exists')
+          const user = await prisma.user.update({
+            where: {
+              id: userId,
+            },
+            data: {
+              balance: {
+                decrement: amount,
+              },
+            },
+            select: {
+              balance: true,
+            },
+          })
+          if (user.balance < 0) throw new BadRequestException('Not Enough Money')
+          transaction = await this.prisma.transaction.create({
+            data: {
+              amount,
+              projectId,
+              userId,
+              type,
+            },
+          })
+          await prisma.project.update({
+            where: {
+              id: projectId,
+            },
+            data: {
+              currentFunding: {
+                increment: amount,
+              },
+            },
+          })
+        } else if (type === TransactionType.refill) {
+          transaction = await prisma.transaction.create({
+            data: {
+              amount,
+              type,
+              userId,
+            },
+          })
+          await prisma.user.update({
+            where: {
+              id: userId,
+            },
+            data: {
+              balance: {
+                increment: amount,
+              },
+            },
+          })
+        }
+
         return { transaction }
       })
       return transaction
